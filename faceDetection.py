3b2090070a389c94804260ddaa7102959e145f11
Ehab El-Sherif <ehabraelsherif@gmail.com>
2018-12-12 11:22:18 +0200

    Save changes

--- faceDetection.py
+++ faceDetection.py
@@ -8,10 +8,13 @@ Created on Sat Nov  3 07:57:07 2018
 
 import cv2
 import numpy as np
+import skimage.io as io
+from skimage.color import rgb2hsv
+from skimage.filters import gaussian
 
 # Import Classifier for Face and Eye Detection
 face_classifier = cv2.CascadeClassifier("haarcascades//haarcascade_frontalface_default.xml")
-eye_classifier = cv2.CascadeClassifier ("haarcascades//haarcascade_eye.xml")
+eye_classifier = cv2.CascadeClassifier("haarcascades//haarcascade_eye.xml")
 mouth_classifier = cv2.CascadeClassifier("haarcascades//haarcascade_smile.xml")
 
 if face_classifier.empty():
@@ -24,29 +27,27 @@ if mouth_classifier.empty():
    raise IOError('Unable to load the mouth cascade classifier xml file')
 
 
-def face_detector (img):
+def face_detector ( img ):
     # Convert Image to Grayscale
-    gray = cv2.cvtColor (img, cv2.COLOR_BGR2GRAY)
-    gray = np.array(gray, dtype='uint8')
-    faces = face_classifier.detectMultiScale (gray,1.1, 3)
+    #gray = cv2.cvtColor (img, cv2.COLOR_BGR2GRAY)
+    #gray = np.array(gray, dtype='uint8')
+    faces = face_classifier.detectMultiScale(img,1.1, 3)
     
     #No Faces
     if faces is ():
         return img
-    
+
     # Given coordinates to detect face and eyes location from ROI
     for (x, y, w, h) in faces:
-        #(x,y) : upper-left corner of the face 
-        #(x+w,y+h) : bottom-right corner of the face
-        #
-        cv2.rectangle (img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+
+        cv2.rectangle(img,(w,y),(x+w,y+h),(0,0,255),2)
         #roi for eyes
         #detect eyes
-        roiE_gray = gray[y: y+h, x: x+w]
-        roiE_color = img[y: y+h, x: x+w]
-        eyes = eye_classifier.detectMultiScale (roiE_gray,1.3,7)
-        for (ex, ey, ew, eh) in eyes:
-            cv2.rectangle(roiE_color,(ex,ey),(ex+ew,ey+eh),(0,0,255),2)
+        #roiE_gray = gray[y: y+h, x: x+w]
+        #roiE_color = img[y: y+h, x: x+w]
+        #eyes = eye_classifier.detectMultiScale (roiE_gray,1.3,7)
+        #for (ex, ey, ew, eh) in eyes:
+        #    cv2.rectangle(roiE_color,(ex,ey),(ex+ew,ey+eh),(0,0,255),2)
             #roi_color = cv2.flip (roi_color, 1)
    #return roi_color
         
@@ -59,12 +60,11 @@ def face_detector (img):
 #         for (mx, my, mw, mh) in mouth:
 #             my=int(my-0.15*h)
 #             cv2.rectangle(roiM_color,(mx,my),(mx+mw,my+mh),(0,255,255),2)
-#            
+#
 # =============================================================================
     
     cv2.imshow('img',img)
-    
-img=cv2.imread('tests//419_2.jpg')
+img = cv2.imread('tests//m(01-32)_gr.jpg')
 
 if img is None:
    raise IOError('Unable to load image file')
--- noise.py
+++ noise.py
@@ -1,3 +1,5 @@
+import utilities
+import scipy.ndimage as sp
 class Noise:
 
   def __init__(self, image):
@@ -11,4 +13,11 @@ class Noise:
   
   #apply gaussian filter when image has additive noise
   def Gaussian(self):
-    return
+    return sp.gaussian_filter(self.__img,5)
+
+img=utilities.io.imread("F:\\Google Drive\\CMP 2020\\3rd Year\\3A\\Image Processing\\Labs\\Lab 1 - STD\\pyramids.jpeg")
+u=utilities.Util(img)
+img=u.grey()
+noise=Noise(img)
+img=noise.Gaussian()
+utilities.io.imshow(img)
